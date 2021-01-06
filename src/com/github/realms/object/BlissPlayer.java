38572b27b651d7bfdad1f003e6453734ff07049d
Shehab <shehabhafez3@gmail.com>
2021-01-06 03:08:30 +0200

    drowning

--- src/com/github/realms/object/BlissPlayer.java
+++ src/com/github/realms/object/BlissPlayer.java
@@ -1,10 +1,7 @@
 package com.github.realms.object;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.server.v1_16_R2.EntityPlayer;
-import net.minecraft.server.v1_16_R2.MinecraftServer;
-import net.minecraft.server.v1_16_R2.PlayerInteractManager;
-import net.minecraft.server.v1_16_R2.WorldServer;
+import net.minecraft.server.v1_16_R2.*;
 import org.bukkit.Location;
 import org.bukkit.Material;
 
@@ -22,6 +19,20 @@ public class BlissPlayer extends EntityPlayer {
 
         if (location.getBlock().getType() == Material.AIR) {
             this.f(Math.floor(locX()) + 0.5, Math.floor(locY()) - 1, Math.floor(locZ()) + 0.5);
+        }
+
+        if (location.getBlock().getType() == Material.WATER) {
+            this.f(Math.floor(locX()) + 0.5, locY() - 0.05, Math.floor(locZ()) + 0.5);
+        }
+
+        if (this.getBukkitEntity().getLocation().getBlock().getType() == Material.WATER) {
+            this.f(Math.floor(locX()) + 0.5, locY() - 0.05, Math.floor(locZ()) + 0.5);
+        }
+
+        location.setY(getHeadY());
+        if (location.getBlock().getType() == Material.WATER) {
+            damageEntity(DamageSource.DROWN, 0.5f);
+
         }
     }
 }
